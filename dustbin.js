2bf35545379e03d8637566fe22b66c4f82a119fc
KARTIK-007-gamer <75831692+KARTIK-007-gamer@users.noreply.github.com>
2021-03-02 21:06:57 +0530

    Add files via upload
    Intial commit

---
+++ dustbin.js
@@ -0,0 +1,14 @@
+class dustbin extends BaseClass
+{
+	constructor(x,y)
+	{
+		this.dustbinWidth=200;
+		this.dustbinHeight=213;
+		this.wallThickness=20;
+
+		this.loadimage("sprties/dustingreen.png");
+	}
+		
+     
+
+}
